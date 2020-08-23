714222340d13be60f3daed30c80f4bda01aa3e5f
Jamil <jamilahmedchowdhury4128@gmail.com>
2020-08-24 00:06:22 +0600

    My practice for lecture 2

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ const path = require("path")
 const express = require("express")
 const app = express()
 const port = process.env.PORT || 3000
-
-const publicDirectoryPath = path.join(__dirname, "../public")
-app.use(express.static(publicDirectoryPath))
-app.listen(port, () => {
-    console.log("Listening to port ", port)
-})
+const publicDirectorypath = path.join(__dirname, "../public")
+app.use(express.static(publicDirectorypath))
+app.listen(port, () => `Server running on port ${port} 🔥`);
