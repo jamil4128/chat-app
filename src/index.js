5dd24e1f931e95ea407770e3c4bef2094a3f3cd6
Jamil <jamilahmedchowdhury4128@gmail.com>
2020-08-24 12:19:23 +0600

    intro to socket.io

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 const path = require("path")
+const http = require("http")
 const express = require("express")
+const socketio = require("socket.io")
+
 const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
 const port = process.env.PORT || 3000
 const publicDirectorypath = path.join(__dirname, "../public")
+
+
+
 app.use(express.static(publicDirectorypath))
-app.listen(port, () => `Server running on port ${port} 🔥`);
+
+io.on("connection", () => {
+    console.log("New website connection")
+})
+server.listen(port, () => `Server running on port ${port} 🔥`);
