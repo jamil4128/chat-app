02054995834b36dd565086d3087787ac2633642b
Jamil <jamilahmedchowdhury4128@gmail.com>
2020-08-27 08:17:20 +0600

    timeStamp

--- public/js/chat.js
+++ public/js/chat.js
@@ -13,18 +13,20 @@ const urlTemplate = document.querySelector("#url-template").innerHTML
 socket.on("message", (message) => {
     console.log(message)
     const html = Mustache.render(messageTemplate, {
-        message: message
+        message: message.text,
+        createdAt: moment(message.createdAt).format("h:mm:ss a")
     })
 
     $messages.insertAdjacentHTML("beforeend", html)
 })
 
-socket.on("locationMessage", (url) => {
-    console.log(url)
-    const URL = Mustache.render(urlTemplate, {
-        url
+socket.on("locationMessage", (message) => {
+    console.log(message)
+    const html = Mustache.render(urlTemplate, {
+        url: message.text,
+        createdAt: moment(message.createdAt).format("h:mm:ss a")
     })
-    $messages.insertAdjacentHTML("beforeend", URL)
+    $messages.insertAdjacentHTML("beforeend", html)
 
 })
 
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const http = require("http")
 const express = require("express")
 const socketio = require("socket.io")
 const Filter = require("bad-words")
+const { generateMessage, generateUrl } = require("./utils/message")
 
 const app = express()
 const server = http.createServer(app)
@@ -16,23 +17,23 @@ app.use(express.static(publicDirectorypath))
 io.on("connection", (socket) => {
     console.log("New website connection")
 
-    socket.emit("message", "Welcome!")
-    socket.broadcast.emit("message", "A new user has joined")
+    socket.emit("message", generateMessage("Welcome!"))
+    socket.broadcast.emit("message", generateMessage("A new user has joined"))
 
     socket.on("sendMessage", (message, callback) => {
         const filter = new Filter()
         if (filter.isProfane(message)) {
             return callback("Profinity is not allowed")
         }
-        io.emit("message", message)
+        io.emit("message", generateMessage(message))
         callback()
 
     })
     socket.on("disconnect", () => {
-        io.emit("message", "A user has left the chat")
+        io.emit("message", generateMessage("A user has left the chat"))
     })
     socket.on("sendLocation", (position, callback) => {
-        io.emit("locationMessage", `https://www.google.com/maps?q=${position.lat},${position.long}`)
+        io.emit("locationMessage", generateUrl(`https://www.google.com/maps?q=${position.lat},${position.long}`))
         callback()
     })
 
